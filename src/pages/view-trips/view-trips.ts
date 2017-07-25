e2badd70a030c33773fbe5223186643a1c7a2204
Arjun Roychowdhury <arjun@hsc.com>
2017-07-25 12:21:52 -0400

    #4 - basic fetch

--- src/pages/view-trips/view-trips.ts
+++ src/pages/view-trips/view-trips.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { CommonUtilsProvider } from '../../providers/common-utils/common-utils';
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import { NgZone } from '@angular/core';
 
 /**
  * Generated class for the ViewTripsPage page.
@@ -14,12 +17,52 @@ import { CommonUtilsProvider } from '../../providers/common-utils/common-utils';
   templateUrl: 'view-trips.html',
 })
 export class ViewTripsPage {
+  trips:any[] = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public utils:CommonUtilsProvider) {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public utils:CommonUtilsProvider, public zone:NgZone) {
+
   }
+
+
+  cloudGetTrips() {
+     let ref = firebase.database().ref('tripDataIndex/');
+     let ltrips:any[] = [];
+     ref.on('value', (snapshot) => {
+      // console.log(snapshot.val());
+      this.utils.presentLoader ("retrieving trips...",-1);
+       let result = snapshot.val();
+       //console.log (JSON.stringify(result));
+       for (let k in result) {
+        ltrips.push({
+          id: k,
+          url: result[k].url,
+          date: result[k].uploadedon
+        });
+
+        console.log ("PUSHING: "+ JSON.stringify(ltrips[0]))
+      }
+      this.zone.run(() => {
+        this.trips = ltrips;
+        this.utils.removerLoader();
+      })
+
+     });
+  }
+
+  cloudGetTripsWithAuth() {
+
+    this.utils.doAuthWithPrompt()
+    .then (succ => {this.cloudGetTrips()})
+    .catch (err => {});
+  }
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ViewTripsPage');
+    this.cloudGetTripsWithAuth();
   }
 
 }
+
+
