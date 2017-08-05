f6343e5c996715f62a35602dd6bef6aff0471cd4
Arjun Roychowdhury <arjun@hsc.com>
2017-08-04 21:26:32 -0400

    updated token

--- changeloggen.sh
+++ changeloggen.sh
@@ -1,4 +1,4 @@
 echo "*** PLEASE MAKE SURE YOU HAVE RUN MAKETAG BEFORE**"
 echo -n  "Enter new version:"
 read newver
-github_changelog_generator  -u hsccorp -p sensorcollect --future-release "$newver" --token 398b9e4b83d5a9969d56c180f18f940cb1a3b8a8
+github_changelog_generator  -u hsccorp -p sensorcollect --future-release "$newver" --token ${SENSORTOKEN}
