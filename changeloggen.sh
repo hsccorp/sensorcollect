1fa44a2a7d50dbde881e9548586de405c1d37f2e
Arjun Roychowdhury <arjun@hsc.com>
2017-08-07 16:16:21 -0400

    dont run maketag before :)

--- changeloggen.sh
+++ changeloggen.sh
@@ -1,4 +1,3 @@
-echo "*** PLEASE MAKE SURE YOU HAVE RUN MAKETAG BEFORE**"
 echo -n  "Enter new version:"
 read newver
 github_changelog_generator  -u hsccorp -p sensorcollect --future-release "$newver" --token ${SENSORTOKEN}
